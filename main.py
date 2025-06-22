07a5e7f605ab66979cedd635cf7482d9626fa5db
Corentin <declan.lowee@gmail.com>
2025-06-23 00:14:17 +0100

    feat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import osmnx as ox
 import networkx as nx
+from itertools import combinations
 
 #Drone:
 #- 100 euro / jour
@@ -13,19 +14,31 @@ G = G.to_undirected()
 
 degrees = G.degree()
 
-odd_degrees = []
+odd_nodes = []
 for node, degree in degrees:
 	if degree % 2 != 0:
-		odd_degrees.append(node)
+		odd_nodes.append(node)
+
 
 odd_G = nx.MultiDiGraph()
 
-dijkstra_result = nx.all_pairs_dijkstra_path_length(odd_G, weight="length")
+shortest_paths = nx.all_pairs_dijkstra_path_length(G, weight="length")
 
-jsp = 0
+for u, v in combinations(odd_nodes, 2):
+	distance = shortest_paths[u][v]
+	odd_G.add_edge(u, v, weight=distance)
+
+matching = nx.algorithms.matching.min_weight_matching(odd_G, maxcardinality=True, weight="weight")
+
+for u, v in matching:
+	path = nx.shortest_path(G, source=u, target=v, weight="length")
+	for i in range(len(path) - 1):
+		G.add_edge(path[i], path[i + 1], length=G[path[i]][path[i + 1]][0]['length'])
+
+circuit = list(nx.eulerian_circuit(G))
 
 
-#ox.plot_graph(G)
+ox.plot_graph(odd_G)
 
-for node, data in G.nodes(data=True):
-	print(f"Node {node}: {data}")
+#for node, data in G.nodes(data=True):
+#	print(f"Node {node}: {data}")
