80afbca1e8b10d1573aae0ab92cb3cd1c27bed51
Allalou Corentin <corentin.guiniot-allalou@interieur.gouv.fr>
2025-06-24 19:55:17 +0200

    fix

--- utils.py
+++ utils.py
@@ -1,33 +1,33 @@
 class Vehicule:
-    def __init__(self, cost, cost_per_km):
-        self.cost = cost
+    def __init__(self, cost_per_day, cost_per_km):
+        self.cost_per_day = cost_per_day
         self.cost_per_km = cost_per_km
 
     def __str__(self):
-        return f"Vehicule(cost={self.cost}, cost_per_km={self.cost_per_km})"
+        return f"Vehicule(cost={self.cost_per_day}, cost_per_km={self.cost_per_km})"
 
 
 class Drone(Vehicule):
-    def __init__(self, cost, cost_per_km):
-        super().__init__(cost, cost_per_km)
+    def __init__(self, cost_per_day, cost_per_km):
+        super().__init__(cost_per_day, cost_per_km)
 
     def __str__(self):
-        return f"Drone(cost={self.cost}, cost_per_km={self.cost_per_km})"
+        return f"Drone(cost={self.cost_per_day}, cost_per_km={self.cost_per_km})"
 
 
 class Snowplow(Vehicule):
     def __init__(
         self,
-        cost,
+        cost_per_day,
         cost_per_km,
         cost_for_first_8hours,
         cost_for_next8hours,
         average_speed,
     ):
-        super().__init__(cost, cost_per_km)
+        super().__init__(cost_per_day, cost_per_km)
         self.cost_for_first_8hours = cost_for_first_8hours
         self.cost_for_next8hours = cost_for_next8hours
         self.average_speed = average_speed
 
     def __str__(self):
-        return f"Snowplow(cost={self.cost}, cost_per_km={self.cost_per_km}, cost_for_first_8hours={self.cost_for_first_8hours}, cost_for_next8hours={self.cost_for_next8hours}, average_speed={self.average_speed})"
+        return f"Snowplow(cost={self.cost_per_day}, cost_per_km={self.cost_per_km}, cost_for_first_8hours={self.cost_for_first_8hours}, cost_for_next8hours={self.cost_for_next8hours}, average_speed={self.average_speed})"
