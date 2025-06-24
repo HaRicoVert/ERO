b1d6b6c19d482ff29a7c465066b5be457b4c8bf0
Allalou Corentin <corentin.guiniot-allalou@interieur.gouv.fr>
2025-06-24 21:14:21 +0200

    maj

--- l’étude du plan de déneigement/main.py
+++ l’étude du plan de déneigement/main.py
@@ -1,10 +1,11 @@
-from random import randint
-
 import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
 import osmnx
 from matplotlib.cm import ScalarMappable
 from matplotlib.lines import Line2D
+
+from utilities.utils import generate_random_snow_levels, MIN_SNOW_LEVEL, MAX_SNOW_LEVEL
+from utilities.utils import get_edge_colors
 
 # multi_query = [
 #     "Outremont",
@@ -16,40 +17,16 @@ from matplotlib.lines import Line2D
 #
 # graphe_outremont = osmnx.graph_from_place(multi_query, network_type="drive")
 
-# Charger le graphe
-graphe_outremont = osmnx.graph_from_place(
+graph = osmnx.graph_from_place(
     "Outremont, Montreal, Quebec, Canada", network_type="drive"
 )
 
-
-# Ajouter les niveaux de neige
-for u, v, k, data in graphe_outremont.edges(keys=True, data=True):
-    data["snow_level"] = randint(0, 15)
-
-
-def get_edge_colors(G):
-    colors = []
-    gray_color = (1, 1, 1, 1.0)  # RGBA pour blanc
-
-    green_yellow = mcolors.LinearSegmentedColormap.from_list(
-        "green_yellow", ["green", "yellow"]
-    )
-
-    for u, v, k, data in G.edges(keys=True, data=True):
-        snow_level = data["snow_level"]
-        if snow_level < 2.5 or snow_level > 15:
-            colors.append(gray_color)
-        else:
-            normalized = (snow_level - 2.5) / (15 - 2.5)
-            colors.append(green_yellow(normalized))
-    return colors
-
+generate_random_snow_levels(graph, min_level=0, max_level=15)
 
 fig, ax = plt.subplots(figsize=(12, 8))
-
-edge_colors = get_edge_colors(graphe_outremont)
+edge_colors = get_edge_colors(graph)
 osmnx.plot_graph(
-    graphe_outremont,
+    graph,
     ax=ax,
     edge_color=edge_colors,
     edge_linewidth=3,
@@ -58,10 +35,9 @@ osmnx.plot_graph(
     close=False,
 )
 
-# Créer la légende avec gradient
-# Colorbar pour la zone d'intérêt (2.5-15 cm)
-norm = mcolors.Normalize(vmin=2.5, vmax=15)
-sm = ScalarMappable(norm=norm, cmap=plt.cm.Greens)
+sm = ScalarMappable(
+    norm=mcolors.Normalize(vmin=MIN_SNOW_LEVEL, vmax=MAX_SNOW_LEVEL), cmap=plt.cm.Greens
+)
 sm.set_array([])
 
 cbar = plt.colorbar(sm, ax=ax, shrink=0.8, aspect=20)
@@ -69,18 +45,29 @@ cbar.set_label("Niveau de neige (cm)", rotation=270, labelpad=20)
 
 legend_elements = [
     Line2D(
-        [0], [0], color="gray", lw=3, label="< 2.5 cm ou > 15 cm (pas de déneigement)"
+        [0],
+        [0],
+        color="gray",
+        lw=3,
+        label=f"< {MIN_SNOW_LEVEL} cm ou > {MAX_SNOW_LEVEL} cm (pas de déneigement)",
     ),
     Line2D(
-        [0], [0], color=plt.cm.Greens(0.3), lw=3, label="2.5 cm (déneigement requis)"
+        [0],
+        [0],
+        color=plt.cm.Greens(0.3),
+        lw=3,
+        label=f"{MIN_SNOW_LEVEL} cm (déneigement requis)",
     ),
     Line2D(
-        [0], [0], color=plt.cm.Greens(1.0), lw=3, label="15 cm (déneigement requis)"
+        [0],
+        [0],
+        color=plt.cm.Greens(1.0),
+        lw=3,
+        label=f"{MAX_SNOW_LEVEL} cm (déneigement requis)",
     ),
 ]
 
 ax.legend(handles=legend_elements, loc="upper right", bbox_to_anchor=(1, 1))
 
 plt.title("Réseau routier d'Outremont - Niveaux de neige")
-plt.tight_layout()
 plt.show()
--- utilities/utils.py
+++ utilities/utils.py
@@ -1,3 +1,10 @@
+import os
+import random
+
+import matplotlib.colors as mcolors
+from dotenv import load_dotenv
+
+
 class Vehicule:
     def __init__(self, cost_per_day, cost_per_km):
         self.cost_per_day = cost_per_day
@@ -7,12 +14,12 @@ class Vehicule:
         return f"Vehicule(cost={self.cost_per_day}, cost_per_km={self.cost_per_km})"
 
 
-class Drone(Vehicule):
+class SuperDrone(Vehicule):
     def __init__(self, cost_per_day, cost_per_km):
         super().__init__(cost_per_day, cost_per_km)
 
     def __str__(self):
-        return f"Drone(cost={self.cost_per_day}, cost_per_km={self.cost_per_km})"
+        return f"SuperDrone(cost={self.cost_per_day}, cost_per_km={self.cost_per_km})"
 
 
 class Snowplow(Vehicule):
@@ -31,3 +38,57 @@ class Snowplow(Vehicule):
 
     def __str__(self):
         return f"Snowplow(cost={self.cost_per_day}, cost_per_km={self.cost_per_km}, cost_for_first_8hours={self.cost_for_first_8hours}, cost_for_next8hours={self.cost_for_next8hours}, average_speed={self.average_speed})"
+
+
+load_dotenv("../.env")
+
+
+SUPER_DRONE = SuperDrone(
+    cost_per_day=float(os.getenv("DRONE_COST_PER_DAY")),
+    cost_per_km=float(os.getenv("DRONE_COST_PER_KM")),
+)
+
+SNOWPLOW_TYPE_1 = Snowplow(
+    cost_per_day=float(os.getenv("SNOWPLOW_TYPE1_COST_PER_DAY")),
+    cost_per_km=float(os.getenv("SNOWPLOW_TYPE1_COST_PER_KM")),
+    cost_for_first_8hours=float(os.getenv("SNOWPLOW_TYPE1_COST_FOR_FIRST_8_HOURS")),
+    cost_for_next8hours=float(os.getenv("SNOWPLOW_TYPE1_COST_FOR_NEXT_8_HOURS")),
+    average_speed=float(os.getenv("SNOWPLOW_TYPE1_AVERAGE_SPEED")),
+)
+
+SNOWPLOW_TYPE_2 = Snowplow(
+    cost_per_day=float(os.getenv("SNOWPLOW_TYPE2_COST_PER_DAY")),
+    cost_per_km=float(os.getenv("SNOWPLOW_TYPE2_COST_PER_KM")),
+    cost_for_first_8hours=float(os.getenv("SNOWPLOW_TYPE2_COST_FOR_FIRST_8_HOURS")),
+    cost_for_next8hours=float(os.getenv("SNOWPLOW_TYPE2_COST_FOR_NEXT_8_HOURS")),
+    average_speed=float(os.getenv("SNOWPLOW_TYPE2_AVERAGE_SPEED")),
+)
+
+MIN_SNOW_LEVEL = float(os.getenv("MIN_SNOW_LEVEL"))
+MAX_SNOW_LEVEL = float(os.getenv("MAX_SNOW_LEVEL"))
+
+
+def generate_random_snow_levels(graph, min_level=0, max_level=15):
+    for u, v, k, data in graph.edges(keys=True, data=True):
+        data["snow_level"] = random.uniform(min_level, max_level)
+
+
+def get_edge_colors(graph):
+    colors = []
+    black_color = (0, 0, 0, 1.0)
+
+    green_yellow = mcolors.LinearSegmentedColormap.from_list(
+        "green_yellow", ["green", "yellow"]
+    )
+
+    for u, v, k, data in graph.edges(keys=True, data=True):
+        snow_level = data["snow_level"]
+        if snow_level < MIN_SNOW_LEVEL or snow_level > MAX_SNOW_LEVEL:
+            colors.append(black_color)
+        else:
+            colors.append(
+                green_yellow(
+                    (snow_level - MIN_SNOW_LEVEL) / (MAX_SNOW_LEVEL - MIN_SNOW_LEVEL)
+                )
+            )
+    return colors
