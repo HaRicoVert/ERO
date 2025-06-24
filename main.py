4b853295d01f62fe82633ec7840336b9391d052f
Allalou Corentin <corentin.guiniot-allalou@interieur.gouv.fr>
2025-06-24 19:54:37 +0200

    maj de la structure

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import random
 
-import osmnx as ox
+import osmnx
 
-import graph as vi
+import graph
 from algo import chinese_postman
 
-G_directed = ox.graph_from_place(
+G_directed = osmnx.graph_from_place(
     "Ville-Marie, Montreal, Quebec, Canada", network_type="drive"
 )
 
 # Définir le point de départ à la Mairie de Montréal
-lat, lon = ox.geocode("275 Rue Notre-Dame Est, Montréal, QC")
-start_node = ox.distance.nearest_nodes(G_directed, lon, lat)
+lat, lon = osmnx.geocode("275 Rue Notre-Dame Est, Montréal, QC")
+start_node = osmnx.distance.nearest_nodes(G_directed, lon, lat)
 
-vi.visualize_snow_level(G_directed, start_node)
+graph.visualize_snow_level(G_directed, start_node)
 
 # On ajoute une quantite de neige aleatoire (de 0 a 15) sur les arretes du graph
 for u, v, k, data in G_directed.edges(keys=True, data=True):
@@ -21,7 +21,5 @@ for u, v, k, data in G_directed.edges(keys=True, data=True):
     data["cm_neige"] = cm_neige
 
 drone_path = chinese_postman(G_directed, start_node)
-
-vi.visualize_euler_path_with_arrows(G_directed, drone_path, start_node)
-
-vi.visualize_snow_level(G_directed, start_node)
+graph.visualize_euler_path_with_arrows(G_directed, drone_path, start_node)
+graph.visualize_snow_level(G_directed, start_node)
