2f47100c969126ab546dfb7f0d2f37b45a20257d
Allalou Corentin <corentin.guiniot-allalou@interieur.gouv.fr>
2025-06-25 15:52:37 +0200

    maj

--- l’étude du plan de déneigement/main.py
+++ l’étude du plan de déneigement/main.py
@@ -3,11 +3,11 @@ import networkx
 import osmnx
 
 from utilities.utils import (
-    generate_random_snow_levels,
     set_legend,
+    generate_random_snow_levels,
 )
 
-sectors = [
+sectors_to_exclude = [
     "Outremont, Montreal, Quebec, Canada",
     "Verdun, Montreal, Quebec, Canada",
     "Anjou, Montreal, Quebec, Canada",
@@ -15,13 +15,37 @@ sectors = [
     "Le Plateau-Mont-Royal, Montreal, Quebec, Canada",
 ]
 
-graphs = {}
-for sector in sectors:
-    graphs[sector] = osmnx.graph_from_place(sector, network_type="drive")
+# Récupérer le graphe complet de Montreal
+montreal_graph = osmnx.graph_from_place(
+    "Montreal, Quebec, Canada", network_type="drive"
+)
 
-graph = networkx.compose_all(list(graphs.values()))
+# Récupérer les graphes des secteurs à exclure
+sectors_graphs = {}
+for sector in sectors_to_exclude:
+    sectors_graphs[sector] = osmnx.graph_from_place(sector, network_type="drive")
+
+# Combiner tous les graphes des secteurs à exclure
+sectors_combined = networkx.compose_all(list(sectors_graphs.values()))
+
+
+# Créer une copie du graphe de Montreal
+montreal_filtered = montreal_graph.copy()
+
+# Retirer les nœuds des secteurs spécifiés du graphe de Montreal
+nodes_to_remove = set(sectors_combined.nodes()) & set(montreal_filtered.nodes())
+montreal_filtered.remove_nodes_from(nodes_to_remove)
+
+# On met snow_level à 0 pour les arêtes du graphe filtré
+for u, v, k, data in montreal_filtered.edges(keys=True, data=True):
+    data["snow_level"] = 0
+
+generate_random_snow_levels(sectors_combined, min_level=0, max_level=15)
+
+graph = networkx.compose(sectors_combined, montreal_filtered)
+
+# Projeter et consolider le graphe filtré
 graph = osmnx.project_graph(graph)
 graph = osmnx.consolidate_intersections(graph, tolerance=20)
-generate_random_snow_levels(graph, min_level=0, max_level=15)
 set_legend(plt, graph)
 plt.show()
